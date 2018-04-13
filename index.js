95ca451b5dacfc295735650bb9fcf1748a5193b4
Johnny Le <johnnylewebdeveloper@gmail.com>
2018-04-12 22:15:42 -0400

    Commit

--- index.js
+++ index.js
@@ -46,3 +46,19 @@ sr.reveal('.info-left', {
           distance:'300px',
           viewFactor: 0.2
         });
+
+          $(function() {
+              // Smooth Scrolling
+              $('a[href*="#"]:not([href="#"])').click(function() {
+                if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                  var target = $(this.hash);
+                  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                  if (target.length) {
+                    $('html, body').animate({
+                      scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                  }
+                }
+              });
+            });
